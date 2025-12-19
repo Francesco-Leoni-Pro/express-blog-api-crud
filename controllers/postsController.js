8697b1187074cc0e8a56259c3884af6fdae18f02
Francesco-Leoni-Pro <francesco.leoni.pro@icloud.com>
2025-12-19 22:17:26 +0100

    feat: milestone 2

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,35 +1,44 @@
-// Index
+const posts = require("../posts");
+
+// Index → restituisce la lista dei post in JSON
 function index(req, res) {
-  res.send("Lista dei post");
+  res.json(posts);
 }
 
-// Show
+// Show → restituisce un singolo post in JSON
 function show(req, res) {
-  const { id } = req.params;
-  res.send(`Mostra il post ${id}`);
+  const id = parseInt(req.params.id);
+  const post = posts[id];
+
+  res.json(post);
 }
 
-// Create
+// Create (ancora solo messaggio)
 function store(req, res) {
   res.send("Creazione nuovo post");
 }
 
-// Update (PUT)
+// Update (PUT) (ancora solo messaggio)
 function update(req, res) {
   const { id } = req.params;
   res.send(`Aggiornamento completo del post ${id}`);
 }
 
-// Modify (PATCH)
+// Modify (PATCH) (ancora solo messaggio)
 function modify(req, res) {
   const { id } = req.params;
   res.send(`Modifica parziale del post ${id}`);
 }
 
-// Delete
+// Delete → elimina il post, stampa lista aggiornata, status 204
 function destroy(req, res) {
-  const { id } = req.params;
-  res.send(`Cancellazione del post ${id}`);
+  const id = parseInt(req.params.id);
+
+  posts.splice(id, 1);
+
+  console.log(posts);
+
+  res.sendStatus(204);
 }
 
 module.exports = {
